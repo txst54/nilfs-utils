97c788b07e8888e8e4fbfc01d1f11caf345c966f
Jeffery Xu <jeffxu@node-0.jeffxu-282083.os-class.emulab.net>
2025-11-29 20:10:34 -0700

    thresholds

--- sbin/policies/nilfs_policy_cost_benefit.c
+++ sbin/policies/nilfs_policy_cost_benefit.c
@@ -76,7 +76,7 @@ static int cb_evaluate(struct nilfs_cleaning_policy *policy,
 	candidate->segnum = segnum;
 	candidate->score = (1.0 - u) * age / (1.0 + u);
 	candidate->metadata = NULL;
-	
+	if (candidate->score <= 1) return 0;
 	return 1;  /* Eligible */
 }
 
